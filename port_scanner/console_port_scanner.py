85462748bae84b371f776c53c562d5ea48de3c29
Kosmostar <kosmostar9r@gmail.com>
2021-04-26 21:21:11 +0300

    port scanner base version

---
+++ port_scanner/console_port_scanner.py
@@ -0,0 +1,38 @@
+import argparse
+from port_scanner import Extractor
+
+
+class PortScannerManager:
+    def __init__(self):
+        self.scanner_parser = argparse.ArgumentParser('Script is defining opened port for host in given range of ips')
+
+    def add_arguments(self):
+        self.scanner_parser.add_argument('-r', '--range_ip', required=True, nargs=2,
+                                         help='Input the first and the last ip address in range.\n'
+                                              'e.g "000.000.000.0 000.000.000.255"')
+        self.scanner_parser.add_argument('-p', '--ports', required=True, nargs='*',
+                                         help='Input ports to check.\n'
+                                              'It may be "1-1000",if u want to check all ports from 1 to 1000,\n'
+                                              'or separated with spaces e.g. "21 22 80".')
+        return self.scanner_parser.parse_args()
+
+    def run(self):
+        args = self.add_arguments()
+        print('Running ...')
+        if any(i.find('-') != -1 for i in args.ports):
+            ports = args.ports[0].split('-')
+            ports = [int(i) for i in range(int(ports[0]), int(ports[-1]) + 1)]
+        else:
+            ports = [int(i) for i in args.ports]
+
+        extractor = Extractor(start_ip=args.range_ip[0],
+                              end_ip=args.range_ip[1], ports=ports)
+        extractor.run()
+
+
+manager = PortScannerManager()
+manager.run()
+
+# input example
+# python3 console_port_scanner.py -r 192.168.1.253 192.168.1.255 -p 80 443
+
---
+++ port_scanner/port_scanner.py
@@ -0,0 +1,70 @@
+import re
+import socket
+import threading
+
+RE_IP = re.compile(r"([0-9]{1,3}[\.]){3}[0-9]{1,3}")
+
+
+class PortScanner(threading.Thread):
+
+    def __init__(self, ip, ports, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ip = ip
+        self.ports = ports
+        self.results = []
+
+    def run(self):
+        for port in self.ports:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+            if sock.connect_ex((self.ip, port)):
+                pass
+            else:
+                result = f'{self.ip} {port} OPEN'
+                self.results.append(result)
+            sock.close()
+
+
+class Extractor:
+
+    def __init__(self, start_ip, end_ip, ports):
+        self.start_ip = start_ip
+        self.end_ip = end_ip
+        self.ports = ports
+        self.ips = {}
+
+    def format_hosts(self):
+        match_start_ip = re.search(RE_IP, self.start_ip)
+        match_end_ip = re.search(RE_IP, self.end_ip)
+        if match_start_ip and match_end_ip:
+            start_ip = match_start_ip[0].split('.')
+            end_ip = match_end_ip[0].split('.')
+            base = start_ip[:-1]
+            end_num = end_ip[-1]
+            start_num = start_ip[-1]
+            while int(start_num) <= int(end_num):
+                ip = '.'.join(base) + f'.{start_num}'
+                ips = []
+                for port in self.ports:
+                    ips.append(port)
+                self.ips[ip] = ips
+                start_num = int(start_num) + 1
+        else:
+            print('incorrect ip address')
+
+    def check_opened(self):
+        scanners = [PortScanner(ip=ip, ports=ports) for ip, ports in self.ips.items()]
+        for scanner in scanners:
+            scanner.start()
+        for scanner in scanners:
+            scanner.join()
+        for scanner in scanners:
+            if scanner.results:
+                for i in scanner.results:
+                    print(i)
+        if all(len(scanner.results) == 0 for scanner in scanners):
+            print('There is no opened ports')
+
+    def run(self):
+        self.format_hosts()
+        self.check_opened()
